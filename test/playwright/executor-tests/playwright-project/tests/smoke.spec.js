d18d68286f8037b3e875a6af17231d7c8f263172
Eric Morin <42588957+archetec@users.noreply.github.com>
2024-12-13 11:21:05 -0500

    Update smoke.spec.js

--- test/playwright/executor-tests/playwright-project/tests/smoke.spec.js
+++ test/playwright/executor-tests/playwright-project/tests/smoke.spec.js
@@ -2,7 +2,7 @@
 const { test, expect } = require("@playwright/test");
 
 test("Smoke 1 - has title", async ({ page }) => {
-  await page.goto("https://www.beneva.ca/en");
+  await page.goto("https://github.com");
 
-  await expect(page).toHaveTitle(/Beneva | Insurance & Financial Services/);
+  // await expect(page).toHaveTitle(/Beneva | Insurance & Financial Services/);
 });
